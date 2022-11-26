ed777c4fc5ee6128343e7555dbfa618171720c01
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-11-26 11:09:40 +0100

    modified the homePage.css

---
+++ src/components/home/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import willLogo from "../../components/home/homeImages/willLogo.jpeg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="hp-nav">
+        <div className="hp-nav-logo">
+          <img src={willLogo} className="hp-will-logo" alt="logo" />
+          <h1 className="hp-logo-text">Perfectwill</h1>
+        </div>
+        <Link to={"/about"} id="ab-about">
+          About
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
