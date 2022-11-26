87e406c961e091f36eda7342612ece40630bf536
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-11-26 13:30:02 +0100

    modified About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,24 +7,35 @@ const About = () => {
       <Navbar />
       <div className="ab-about-div">
         <h1 id="ab-about-title">What we do</h1>
-        <p className="ab-text">
-          Perfectwill is a team that offers guidance and directives to testators
-          on how to write their last will and testament regarding how they want
-          their assets distributed among their loved ones
-        </p>
-        <p className="ab-text">
-          We add value by making sure the will is clearly drafted in accordance
-          with the testator's wishes, and that your will is executed in
-          accordance with applicable laws so that your will is valid and reduce
-          the risk of a successful challenge.
-        </p>
-        <p className="ab-text">
-          We make it possible and easy for you to appoint someone to manage your
-          financial affairs if you become incapacitated or otherwise unable to
-          manage your financial affairs on your own. We can also prepare the
-          Power of Attorney, Health Care Proxy, Living Will, Self-Proving
-          Affidavit, Trust Agreement and related documents.
-        </p>
+        {/* <div> */}
+          <div className="ab-text-wrapper">
+            <div>
+              <p className="ab-text">
+                Perfectwill is a team that offers guidance and directives to
+                testators on how to write their last will and testament
+                regarding how they want their assets distributed among their
+                loved ones
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className="ab-text">
+              We add value by making sure the will is clearly drafted in
+              accordance with the testator's wishes, and that your will is
+              executed in accordance with applicable laws so that your will is
+              valid and reduce the risk of a successful challenge.
+            </p>
+          </div>
+          <div>
+            <p className="ab-text">
+              We make it possible and easy for you to appoint someone to manage
+              your financial affairs if you become incapacitated or otherwise
+              unable to manage your financial affairs on your own. We can also
+              prepare the Power of Attorney, Health Care Proxy, Living Will,
+              Self-Proving Affidavit, Trust Agreement and related documents.
+            </p>
+          </div>
+        {/* </div> */}
       </div>
     </div>
   );
--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -4,18 +4,21 @@ import willLogo from "../../components/home/homeImages/willLogo.jpeg";
 
 const Navbar = () => {
   return (
-    <div>
+    
       <nav className="hp-nav">
         <div className="hp-nav-logo">
           <img src={willLogo} className="hp-will-logo" alt="logo" />
           <h1 className="hp-logo-text">Perfectwill</h1>
         </div>
+        <Link to={"/"} id="home">
+          Home
+        </Link>
         <Link to={"/about"} id="ab-about">
           About
         </Link>
         
       </nav>
-    </div>
+    
   );
 };
 
